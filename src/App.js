9c9522741693f4140636e8bcd3c2702109193a82
Utkarsh Sahu <utkarsh.sahu@gammaedge.io>
2025-02-11 14:53:09 +0530

    [2984926] second commit

--- src/App.js
+++ src/App.js
@@ -98,3 +98,10 @@ class TodoApp extends Component {
 }
 
 export default TodoApp;
+
+
+// if (updatedText !== null && updatedText.trim() !== '') {
+  
+//   const updatedTodos = this.state.todos.map((todo) =>
+//     todo.id === id ? { ...todo, text: updatedText } : todo
+//   )}
--- src/edit.js
+++
@@ -1,5 +0,0 @@
-if (updatedText !== null && updatedText.trim() !== '') {
-  
-    const updatedTodos = this.state.todos.map((todo) =>
-      todo.id === id ? { ...todo, text: updatedText } : todo
-    )}
